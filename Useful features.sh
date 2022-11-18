ff34cdd21c16478fa1f738a8b1bcfb7fcc84c9b5
anatolkavassermann <38395431+anatolkavassermann@users.noreply.github.com>
2022-11-18 20:39:41 +0300

    Create Useful features.sh

---
+++ Useful features.sh
@@ -0,0 +1,37 @@
+#extract certs from sig
+openssl pkcs7 -engine gost -inform PEM -in cms.sig -print_certs | awk '
+BEGIN {
+    flag_begin=0; flag_proceed=0; flag_end=0; n=0; filename="";
+} 
+{
+    if ((match ($0, "subject")) != 0) {
+        split($0, temp, ","); 
+        for (a in temp) {
+            if ((match (temp[a], "CN")) != 0) {
+                split(temp[a], tmp, "=");  
+                filename=(tmp[(length(tmp))]);
+                filename=substr(filename,2);
+                gsub(" ", "_", filename);
+            };
+        }
+    };
+    if ($0 == "-----BEGIN CERTIFICATE-----") {flag_begin=1; flag_end=0;};
+    if ($0 == "-----END CERTIFICATE-----") {flag_begin=0; flag_end=1;};
+    if (($0 != "-----BEGIN CERTIFICATE-----") && ($0 != "-----END CERTIFICATE-----") && (flag_end==1)) {flag_begin=0; flag_end=0;};
+    if ((flag_begin==1) && (flag_proceed==0) && (flag_end==0)) {n++;};
+    if (flag_begin==1) {flag_proceed=1;};
+    if (flag_end==1) {flag_proceed=0;};
+    if ((flag_proceed==1) || (flag_end==1)) {print ($0)> "cert_" n "_CN_" filename ".pem";};
+}'
+
+#extract signed content from base64 encoded sig
+cat sig.sig | base64 --decode --ignore-garbage | openssl cms -cmsout -inform DER -print -nameopt utf8 -print | awk '
+BEGIN {
+flag_start=0; flag_proceed=0;
+}
+{
+if (match($0, "eContent:")!=0) {flag_start=1}
+if ((flag_start==1) && (match($0, "        ")!=0)) {flag_proceed=1}
+if ((flag_proceed==1) && (match($0, "        ")==0)) {flag_start=0; flag_proceed=0}
+if (flag_proceed) {for (i=3; i<=16; i++) {printf ($i)}; print ""}
+}' | sed "s|[.]||g" | xxd -r -p
